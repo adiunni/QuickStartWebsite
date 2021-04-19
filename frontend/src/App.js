f8293b73f07f96063920c8ea37e4fb3f9d9a17bb
CharieBlastX7 <61608668+CharieBlastX7@users.noreply.github.com>
2021-04-19 22:59:40 +0530

    Changes made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import { Navbar} from 'react-bootstrap';
+import { Media, Navbar} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useEffect, useState } from 'react';
 
@@ -11,18 +11,23 @@ function App() {
 
 
   return (
-    <div>
+    <div style={{
+      backgroundColor:'red',
+      width:'1440px',
+      height:'700px'
+    }}>
       <Navbar bg="primary" expand="xs" class="navbar-brand navbar sticky-top">
         <Navbar.Brand href="#home" class="text-white">Image Column Display</Navbar.Brand>
       </Navbar>
+      <p className="text-center">To see the images, run the Backend server </p>
       <div class='row justify-content-center'>
-        <div class="col-md-12">
-          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
-          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
-          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
-          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
-          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
-          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
+        <div class="col-md-4">
+          <img src="http://localhost:4000/images/canvas.png" alt="Stupidly made image" class="mr-4 mt-4"></img>
+          <img src="http://localhost:4000/images/canvas(1).png" alt="Stupidly made image" class="ml-4 mt-4"></img>
+          <img src="http://localhost:4000/images/canvas(2).png" alt="Stupidly made image" class="mr-4 mt-4"></img>
+          <img src="http://localhost:4000/images/canvas(3).png" alt="Stupidly made image" class="ml-4 mt-4"></img>
+          <img src="http://localhost:4000/images/canvas(4).png" alt="Stupidly made image" class="mr-4 mt-4"></img>
+          <img src="http://localhost:4000/images/canvas(5).png" alt="Stupidly made image" class="ml-4 mt-4"></img>
         </div>
       </div>
     </div>
