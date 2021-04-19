fcaeb40767b332959ce5eca50a22b13330a6b899
CharieBlastX7 <61608668+CharieBlastX7@users.noreply.github.com>
2021-04-19 22:15:03 +0530

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+import './App.css';
+import { Navbar} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect, useState } from 'react';
+
+
+function App() {
+
+  const [image, setImage] = useState()
+
+
+  return (
+    <div>
+      <Navbar bg="primary" expand="xs" class="navbar-brand navbar sticky-top">
+        <Navbar.Brand href="#home" class="text-white">Image Slideshow</Navbar.Brand>
+      </Navbar>
+      <div class='row justify-content-center'>
+        <div class="col-md-12">
+          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
+          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
+          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
+          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
+          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
+          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="Stupidly made image"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Image.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class Image extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            img:''
+        }
+    }
+}
