7e4e35d6e7edb1a8c95868dd7d673ac6407e2da9
CharieBlastX7 <61608668+CharieBlastX7@users.noreply.github.com>
2021-04-19 22:19:53 +0530

    addition of files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       <Navbar bg="primary" expand="xs" class="navbar-brand navbar sticky-top">
-        <Navbar.Brand href="#home" class="text-white">Image Slideshow</Navbar.Brand>
+        <Navbar.Brand href="#home" class="text-white">Image Column Display</Navbar.Brand>
       </Navbar>
       <div class='row justify-content-center'>
         <div class="col-md-12">
